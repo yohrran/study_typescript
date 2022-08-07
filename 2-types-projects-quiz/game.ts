10adb6a5730eb646e5b7d67c2de7d416aa8893bb
yohrran <johnyohan93@gmail.com>
2022-08-07 18:55:11 +0900

    make direction function

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,35 @@
 /**
  * Let's make a game 🕹
  */
+interface Position {
+  x: number;
+  y: number;
+}
+let position: Position = { x: 0, y: 0 };
+
+type Direction = "up" | "down" | "left" | "right";
+
+function move(direction: Direction) {
+  switch (direction) {
+    case "up":
+      return position.y++;
+    case "down":
+      return position.y--;
+    case "left":
+      return position.x--;
+    case "right":
+      return position.x++;
+    default:
+      throw new Error("👾Error👾");
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
