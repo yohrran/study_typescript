b5aadce032836ac28cbf0b12034764725c372845
yohrran <johnyohan93@gmail.com>
2022-08-07 19:00:21 +0900

    make loading function

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -3,24 +3,40 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  function printLoginState({ state }: ResourceLoadState) {
+    switch (state) {
+      case "loading":
+        console.log("👀 loading...");
+        break;
+      case "success":
+        console.log("😃 loaded");
+        break;
+      case "fail":
+        console.log("😱 no network");
+        break;
+      default:
+        throw new Error("에러가 발생했습니다.");
+    }
+  }
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
