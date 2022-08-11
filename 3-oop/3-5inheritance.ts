17afb93afe9afcfaadf4c6e4c6d859e25eb27adf
yohrran <johnyohan93@gmail.com>
2022-08-11 23:17:49 +0900

    inheritance

---
+++ 3-oop/3-5inheritance.ts
@@ -0,0 +1,81 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log("grinding beans for" + shots);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat() {
+      console.log("heating up....");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling Coffee....${shots}`);
+      return {
+        shots: shots,
+        hasMilk: false,
+      };
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+    clean() {
+      console.log("청소");
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+    private steamMilk() {
+      return "우유";
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  const machine = new CoffeeMachine(23);
+  const machine2 = new CaffeLatteMachine(23, "2323");
+
+  console.log(machine);
+  console.log(machine2.makeCoffee(3));
+  console.log(machine2.serialNumber);
+}
---
+++ 3-oop/3-6polymorphism.ts
@@ -0,0 +1,73 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  //private을 사용하면 외부에서 볼 수 없다
+  //public은 안붙여도 암묵적으로 붙어있다.
+  class CoffeeMachine {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log("grinding beans for" + shots);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat() {
+      console.log("heating up....");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling Coffee....${shots}`);
+      return {
+        shots: shots,
+        hasMilk: false,
+      };
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class caffeLatteMachine extends CoffeeMachine {}
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const make = CoffeeMachine.makeMachine(32);
+  console.log(make);
+  make.fillCoffeeBeans(20);
+  console.log(make.makeCoffee(2));
+  const coffee1 = SweetCoffeeMaker.makeMachine(32);
+  console.log(coffee1.makeCoffee(2));
+}
