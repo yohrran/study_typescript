7333ae36e6f15ef382aff66ee306ea26cb67514e
yohrran <johnyohan93@gmail.com>
2022-08-21 19:00:24 +0900

    Pick type

---
+++ 9-advanced/9-7pick.ts
@@ -0,0 +1,27 @@
+{
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: "videl",
+      url: "https//naver.com",
+      data: "byte-data...",
+    };
+  }
+
+  //원하는 타입만 빼내어서 사용하는 것
+  type VideoMetaData = Pick<Video, "id" | "title">;
+
+  function getVideoMetadata(id: string): VideoMetaData {
+    return {
+      id: id,
+      title: "title",
+    };
+  }
+}
