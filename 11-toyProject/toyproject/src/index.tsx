e76f619e305455807cfa544e70f026744170ad10
yohrran <johnyohan93@gmail.com>
2022-08-31 23:33:30 +0900

    react dnd index.jsx 파일에 적용

--- 11-toyProject/toyproject/src/index.tsx
+++ 11-toyProject/toyproject/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <DndProvider backend={HTML5Backend}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </DndProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
